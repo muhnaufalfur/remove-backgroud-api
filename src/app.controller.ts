5f0e68a80a3e97ad289c55f4092b54ec12402145
naufalfur <muhammadnaufal.furqon@wingscorp.com>
2025-09-24 10:00:49 +0700

    change hellow world handler

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,11 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    return {
+      message: 'Connected to Remove Background API',
+      timezone: process.env.TZ,
+      serverTime: new Date().toLocaleString(),
+    }
   }
 }
